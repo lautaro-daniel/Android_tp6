c1f08a8fc149fc3c4fa7c523c8c65a180d2c4c7b
lautaro-daniel <lauty.9984@gmail.com>
2023-04-30 21:33:32 -0300

    doing changes

--- app/src/main/java/com/pil/movieApp/service/MovieRequestGenerator.kt
+++ app/src/main/java/com/pil/movieApp/service/MovieRequestGenerator.kt
@@ -1,27 +1,33 @@
 package com.pil.movieApp.service
 
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object MovieRequestGenerator {
-    private const val API_MOVIES_URL = "https://api.themoviedb.org/"
-    private const val API = "apikey"
+    private const val API_MOVIE_URL = "https://api.themoviedb.org"
+    private const val accessToken =
+        "eyJhbGciOiJIUzI1NiJ9." +
+                "eyJhdWQiOiI3ZDNlNmE1YzQ1ZWJmNGZlYTJmYTQ3YmIwNDZmODc1NiIsInN1YiI6Ij" +
+                "Y0NGVlYWMwMmZjY2VlMDJlNGNkNzE1ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJ" +
+                "zaW9uIjoxfQ." +
+                "8O73pnC3L0twnDdZpcZQJpauecCSmY5Mb6Pr-_Nlewo"
+    class OAuthInterceptor(private val tokenType: String, private val accessToken: String) : Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response {
+            var request = chain.request()
+            request = request.newBuilder().header("Authorization", "$tokenType $accessToken").build()
 
-    //fix
-    private val httpClient = OkHttpClient.Builder().addInterceptor { chain ->
-        val defaultRequest = chain.request()
-        val defaultHttpUrl = defaultRequest.url()
-        val httpUrl = defaultHttpUrl.newBuilder()
-            .addQueryParameter(API,"api_key=7d3e6a5c45ebf4fea2fa47bb046f8756")
-            .build()
-        val requestBuilder = defaultRequest.newBuilder().url(httpUrl)
-        chain.proceed(requestBuilder.build())
+            return chain.proceed(request)
+        }
     }
 
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor(OAuthInterceptor("Bearer", accessToken))
 
     private val builder = Retrofit.Builder()
-        .baseUrl(API_MOVIES_URL)
+        .baseUrl(API_MOVIE_URL)
         .addConverterFactory(GsonConverterFactory.create())
 
     fun <S> createService(serviceClass: Class<S>): S {
