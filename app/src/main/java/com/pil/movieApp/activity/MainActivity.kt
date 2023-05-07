e0a3af1cf15689dc6b5a88c3a14e2c534a3e7b7d
lautaro-daniel <lauty.9984@gmail.com>
2023-05-07 03:20:10 -0300

    fix bugs

--- app/src/main/java/com/pil/movieApp/activity/MainActivity.kt
+++ app/src/main/java/com/pil/movieApp/activity/MainActivity.kt
@@ -2,7 +2,6 @@ package com.pil.movieApp.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.pil.retrofit_room.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
@@ -15,7 +14,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //binding.buttonBackHome.visibility = View.INVISIBLE
 
         binding.buttonGetMovies.setOnClickListener {
             val intent = Intent(this, MovieActivity::class.java)
